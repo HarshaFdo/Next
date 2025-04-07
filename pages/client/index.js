b552d40a26d570bd2f58bd67843ade9056ea698c
HarshaFdo <fernando.harsha2016@gmail.com>
2025-04-07 16:49:53 +0530

    route pages

---
+++ pages/client/index.js
@@ -0,0 +1,13 @@
+import { useRouter } from "next/router";
+
+function ClientsPage() {
+  const router = useRouter;
+  
+  return (
+    <div>
+      <h1>The Client Page</h1>
+    </div>
+  )
+}
+
+export default ClientsPage;
